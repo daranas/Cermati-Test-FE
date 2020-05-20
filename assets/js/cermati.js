1377999b6c63e47cd0be18aa7146bc4fb3387769
Darana Vidya <darana.sv@gmail.com>
2020-05-20 19:42:12 +0700

    change animation for newsletter

--- assets/js/cermati.js
+++ assets/js/cermati.js
@@ -1,8 +1,22 @@
 $( document ).ready(function() {
+  // notif
   var notifBar = $('.notification');
   var sectionHero = $('.hero');
   $('#getNotif').on('click', function() {
-    notifBar.slideUp(80);
+    notifBar.slideUp(120);
     sectionHero.css('padding-top', 0);
+  });
+
+  // newsletter
+  var newsletter = $('.newsletter');
+  var closeNewsletter = $('.newsletter__close');
+  $(window).scroll(function() {
+    if ($(window).scrollTop()  > $(window).height() / 3) {        
+      newsletter.slideDown(450);
+    }
+  });
+
+  closeNewsletter.on('click', function() {
+    newsletter.slideUp(450);
   })
 });
